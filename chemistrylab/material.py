2473ebc5a43e99b07c62597881131b0f3f024107
Chris Beeler <christopher.beeler@uoit.net>
2019-11-12 14:38:35 -0500

    Added materials

--- chemistrylab/material.py
+++ chemistrylab/material.py
@@ -16,13 +16,13 @@ class Material:
                  ):
         self.name = name
         self.w2d = None
-        self.density = density
+        self.density = density # g/mL
         self.polarity = polarity
-        self.temperature = temperature
-        self.pressure = pressure
+        self.temperature = temperature # K
+        self.pressure = pressure # kPa
         self.phase = phase
         self.charge = charge
-        self.molar_mass = molar_mass
+        self.molar_mass = molar_mass # g/mol
         self.color = color
 
     def _update_properties(self,
@@ -48,6 +48,11 @@ class Material:
 class Air(Material):
     def __init__(self):
         super().__init__(name='Air',
+                         density=1.225e-3,
+                         temperature=297,
+                         pressure=1,
+                         phase='g',
+                         molar_mass=28.963,
                          color=0.45
                          )
 
@@ -57,10 +62,10 @@ class H2O(Material):
         super().__init__(name='H2O',
                          density=0.997,
                          polarity=2 * 1.24 * np.cos((109.5 / 2) * (np.pi / 180.0)),
-                         temperature=25,
+                         temperature=298,
                          pressure=1,
                          phase='l',
-                         molar_mass=18.01528,
+                         molar_mass=18.015,
                          color=0.2,
                          charge=0.0
                          )
@@ -71,21 +76,127 @@ class C6H14(Material):
         super().__init__(name='C6H14',
                          density=0.655,
                          polarity=0.0,
-                         temperature=25,
+                         temperature=298,
                          pressure=1,
                          phase='l',
-                         molar_mass=86.18,
+                         molar_mass=86.175,
                          color=0.65,
                          charge=0.0
                          )
 
+class NaCl(Material):
+    def __init__(self):
+        super().__init__(name='NaCl',
+                         density=2.165,
+                         polarity=1.5,
+                         temperature=298,
+                         pressure=1,
+                         phase='s',
+                         molar_mass=58.443,
+                         color=0.9,
+                         charge=0.0
+                         )
+
+# Polarity is dependant on charge for atoms
+class Na(Material):
+    def __init__(self):
+        super().__init__(name='Na',
+                         density=0.968,
+                         polarity=0.0,
+                         temperature=298,
+                         pressure=1,
+                         phase='s',
+                         molar_mass=22.990,
+                         color=0.85,
+                         charge=0.0
+                         )
+
+# Note: Cl is very unstable when not an aqueous ion
+class Cl(Material):
+    def __init__(self):
+        super().__init__(name='Cl',
+                         density=3.214e-3,
+                         polarity=0.0,
+                         temperature=298,
+                         pressure=1,
+                         phase='g',
+                         molar_mass=35.453,
+                         color=0.8,
+                         charge=0.0
+                         )
+
+class Cl2(Material):
+    def __init__(self):
+        super().__init__(name='Cl2',
+                         density=2.898e-3,
+                         polarity=0.0,
+                         temperature=298,
+                         pressure=1,
+                         phase='g',
+                         molar_mass=70.906,
+                         color=0.8,
+                         charge=0.0
+                         )
+
+class LiF(Material):
+    def __init__(self):
+        super().__init__(name='LiF',
+                         density=2.640,
+                         polarity=1.5,
+                         temperature=298,
+                         pressure=1,
+                         phase='s',
+                         molar_mass=25.939,
+                         color=0.9,
+                         charge=0.0
+                         )
+
+class Li(Material):
+    def __init__(self):
+        super().__init__(name='Li',
+                         density=0.534,
+                         polarity=0.0,
+                         temperature=298,
+                         pressure=1,
+                         phase='s',
+                         molar_mass=6.941,
+                         color=0.95,
+                         charge=0.0
+                         )
+
+# Note: F is very unstable when not an aqueous ion
+class F(Material):
+    def __init__(self):
+        super().__init__(name='F',
+                         density=1.696e-3,
+                         polarity=0.0,
+                         temperature=298,
+                         pressure=1,
+                         phase='g',
+                         molar_mass=18.998,
+                         color=0.8,
+                         charge=0.0
+                         )
+
+class F2(Material):
+    def __init__(self):
+        super().__init__(name='F2',
+                         density=1.696e-3,
+                         polarity=0.0,
+                         temperature=298,
+                         pressure=1,
+                         phase='g',
+                         molar_mass=37.997,
+                         color=0.8,
+                         charge=0.0
+                         )
 
 class T1(Material):
     def __init__(self):
         super().__init__(name='temp1',
                          density=0.655,
                          polarity=0.9,
-                         temperature=25,
+                         temperature=298,
                          )
 
 
@@ -94,5 +205,5 @@ class T2(Material):
         super().__init__(name='temp2',
                          density=0.655,
                          polarity=0.1,
-                         temperature=25,
+                         temperature=298,
                          )
