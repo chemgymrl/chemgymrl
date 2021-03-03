578664d173540c2639d4e81f972b18cca732f777
Mark Baula <57877999+mbaula@users.noreply.github.com>
2021-03-03 11:35:27 -0500

    fixed plotting issue for boiling materials

--- chemistrylab/distillations/distill_v0.py
+++ chemistrylab/distillations/distill_v0.py
@@ -180,7 +180,9 @@ class Distillation:
                 label="beaker_{}".format(i),
                 temperature=293.15, # room temp in Kelvin
                 v_max=self.max_vessel_volume,
-                default_dt=self.dt
+                default_dt=self.dt,
+                materials={},  # moles of materials
+                solutes={}  # moles of solutes
             )
             vessels.append(beaker)
 
