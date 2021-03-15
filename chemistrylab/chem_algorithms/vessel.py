48baebb13835ff44e0b1b6f98caa478d141716ff
mitchellshahen <mitchell.shahen@gmail.com>
2021-03-15 12:59:44 -0400

    Added Pressure and Part Pressure as Vessel Functions

--- chemistrylab/chem_algorithms/vessel.py
+++ chemistrylab/chem_algorithms/vessel.py
@@ -1186,6 +1186,38 @@ class Vessel:
 
         return max_pressure
 
+    def get_pressure(self):
+        '''
+        Method to get the vessel's maximal pressure property
+        '''
+
+        # set up a variable to contain the total pressure
+        total_pressure = 0
+
+        # calculate the total pressure in a vessel using the material dictionary
+        for item in self._material_dict.items():
+            total_pressure += item[1][1] * R * self.temperature / self.volume
+
+        # if the vessel contains no material use 1 atm as a baseline (in kPa)
+        if total_pressure == 0:
+            total_pressure = 101.325
+
+        return total_pressure
+
+    def get_part_pressure(self):
+        '''
+        Method to get the vessel's maximal pressure property
+        '''
+
+        # set up a variable to contain the partial pressures
+        part_pressures = np.zeros(len(self._material_dict))
+
+        # calculate the total pressure in a vessel using the material dictionary
+        for i, item in enumerate(self._material_dict.items()):
+            part_pressures[i] = item[1][1] * R * self.temperature / self.volume
+
+        return part_pressures
+
     def get_defaultdt(self):
         '''
         Method to get the vessel's default time-step property
@@ -1199,21 +1231,6 @@ class Vessel:
         '''
 
         return self.temperature
-
-    def get_pressure(self):
-        '''
-        Method to get the vessel's pressure property
-        '''
-
-        # set up a variable to contain the total pressure
-        total_pressure = 0
-
-        # calculate the total pressure in a vessel using the material dictionary
-        for item in self._material_dict.items():
-            print(item)
-            total_pressure += item[1][1] * R * self.temperature / self.volume
-
-        return total_pressure
 
     def get_volume(self):
         '''
