d4ab80324b8dd092cdc800d27656fe4dc4eac51b
mbaula <markbaula.1@gmail.com>
2021-03-26 16:06:39 -0400

    fixed parameters default value

--- chemistrylab/chem_algorithms/material.py
+++ chemistrylab/chem_algorithms/material.py
@@ -84,7 +84,7 @@ class Material:
     def get_name(self):
         return self._name
 
-    def get_density(self, convert_to_g_cubic_meter):
+    def get_density(self, convert_to_g_cubic_meter=False):
         # need to convert to g/m^3 in order to get volume in litres
         if convert_to_g_cubic_meter:
             return self._density * 1000000
