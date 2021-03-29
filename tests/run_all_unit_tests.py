813c0d896fa60c1697bcdb00d8159845f0ab2bf7
nicholas paquin <napaquin@uwaterloo.ca>
2021-03-29 12:11:15 -0400

    more test functions

--- tests/run_all_unit_tests.py
+++ tests/run_all_unit_tests.py
@@ -4,3 +4,4 @@ for dir in os.listdir('./unit'):
     os.system(f'cd {os.path.join("./unit", dir)}')
     os.system('python3 -m unittest discover -p "*_test.py"')
     os.system('python3 -m unittest discover -p "test_*.py"')
+    os.system('cd ..')
---
+++ tests/unit/chem_algorithms/test_vessel.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+import sys
+import numpy as np
+sys.path.append("../../") # to access `chemistrylab`
+from chemistrylab.chem_algorithms import material, util
+from chemistrylab.chem_algorithms.vessel import Vessel
+
+
+class TestVessel(TestCase):
+    def test__update_materials(self):
+        vessel = Vessel("test")
+        vessel.push_event_to_queue()
+
+    def test__update_temperature(self):
+        self.fail()
+
+    def test__change_heat(self):
+        self.fail()
+
+    def test__pour_by_volume(self):
+        self.fail()
+
+    def test__drain_by_pixel(self):
+        self.fail()
+
+    def test__fully_mix(self):
+        self.fail()
+
+    def test__update_material_dict(self):
+        self.fail()
+
+    def test__update_solute_dict(self):
+        self.fail()
+
+    def test__mix(self):
+        self.fail()
+
+    def test__update_layers(self):
+        self.fail()
