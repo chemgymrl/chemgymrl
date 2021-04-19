fixed errors with rate scaling etc :)

chemistrylab/lab/de.py
@@ -187,7 +187,7 @@ class De:
         n_reactions = activ_energy_arr.shape[0]
 
         # acquire the concentrations of the reactants from the full concentration array
-        reactant_conc = conc
+        reactant_conc = conc[:self.num_reagents]
 
         # only non-zero reactant concentrations will be of use, so these are isolated
         non_zero_conc = np.array([conc for conc in reactant_conc if conc != 0.0])
@@ -208,7 +208,7 @@ class De:
             rate_constant_dim = rate_exponential - 1
 
             # get the dimensionality of the aggregate concentration
-            agg_conc_dim = len(non_zero_conc)
+            agg_conc_dim = len(non_zero_conc) if len(non_zero_conc) != 0 else 1
 
             # calculate the exponential needed to be applied to the aggregate concentration
             # to properly define the rate constant's pre-exponential factor
@@ -221,9 +221,11 @@ class De:
             scaling_arr[i] = scaling_factor
 
         # use the activation energy and scaling factor arrays to calculate the rate constants for each reaction
+        assert 0 not in scaling_arr
+        assert np.nan not in scaling_arr
+        assert np.inf not in scaling_arr
         scaling_coefficients = 1 / scaling_arr
         k = scaling_coefficients * np.exp((-1.0 * activ_energy_arr) / (R * temp))
-        k = np.array([1.5, 0.4])
 
         return k
 

chemistrylab/reactions/available_reactions/chloro_wurtz_v1.py
@@ -46,7 +46,7 @@ DESIRED = "4,5-diethyloctane"
 
 # add the initial thermodynamic values
 Ti = 297.0 # in Kelvin
-Vi = 0.002 # in Litres
+Vi = 1 # in Litres
 
 # additional vessel parameters
 dt = 0.01
@@ -54,20 +54,20 @@ Tmin = 250.0
 Tmax = 500.0
 dT = 50.0
 Vmin = 0.001
-Vmax = 0.005
-dV = 0.0005
+Vmax = 2
+dV = 0.005
 
 # add the arrays containing rate calculation parameters; these include:
     # the activation energies for each reaction (6x1 array)
     # the stoichiometric coefficients (6x4 array)
 activ_energy_arr = np.array(
     [
-        1, # activation energy for reaction 0
-        1, # activation energy for reaction 1
-        1, # activation energy for reaction 2
-        1, # activation energy for reaction 3
-        1, # activation energy for reaction 4
-        1  # activation energy for reaction 5
+        500, # activation energy for reaction 0
+        1000, # activation energy for reaction 1
+        750, # activation energy for reaction 2
+        900, # activation energy for reaction 3
+        1100, # activation energy for reaction 4
+        600  # activation energy for reaction 5
     ]
 )
 