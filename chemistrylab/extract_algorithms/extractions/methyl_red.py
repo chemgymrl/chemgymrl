b58a65b47134ab0e9c60d1fe8b5ce1504291e4c4
beelerchris <chrisbeeler25@gmail.com>
2021-11-04 17:45:43 -0400

    Added get_observation_space to extrations

--- chemistrylab/extract_algorithms/extractions/methyl_red.py
+++ chemistrylab/extract_algorithms/extractions/methyl_red.py
@@ -58,11 +58,11 @@ class Extraction:
         self.target_material = target_material
         self.target_material_init_amount = extraction_vessel.get_material_amount(target_material)
 
-    # def get_observation_space(self):
-    #     obs_low = np.zeros((self.n_total_vessels, self.n_vessel_pixels), dtype=np.float32)
-    #     obs_high = 1.0 * np.ones((self.n_total_vessels, self.n_vessel_pixels), dtype=np.float32)
-    #     observation_space = gym.spaces.Box(obs_low, obs_high, dtype=np.float32)
-    #     return observation_space
+    def get_observation_space(self):
+        obs_low = np.zeros((self.n_total_vessels, self.n_vessel_pixels), dtype=np.float32)
+        obs_high = 1.0 * np.ones((self.n_total_vessels, self.n_vessel_pixels), dtype=np.float32)
+        observation_space = gym.spaces.Box(obs_low, obs_high, dtype=np.float32)
+        return observation_space
 
     def get_action_space(self):
         '''
