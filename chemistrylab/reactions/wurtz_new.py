b270e75f11ca957768ee57c595fbab5d8527905d
beelerchris <chrisbeeler25@gmail.com>
2022-04-21 14:31:02 -0400

    Solvent vs solute corrections

--- chemistrylab/reactions/wurtz_new.py
+++ chemistrylab/reactions/wurtz_new.py
@@ -38,7 +38,7 @@ PRODUCTS = [
     "NaCl"
 ]
 ALL_MATERIALS = REACTANTS + PRODUCTS
-SOLUTES = ["H2O"]
+SOLVENTS = ["H2O"]
 
 rate_coef = np.array([[2, 0, 0, 1],
                       [1, 1, 0, 1],
@@ -57,17 +57,18 @@ reaction_coef = np.array([[-2, -1, -1, 0, 0, 0],
                           [0, 0, 0, 1, 0, 0],
                           [0, 0, 0, 0, 1, 0],
                           [0, 0, 0, 0, 0, 1],
-                          [2, 2, 2, 2, 2, 2]])
+                          [2, 2, 2, 2, 2, 2],
+                          [0, 0, 0, 0, 0, 0]])
 
 wurtz_de = De(rate_coef, np.ones(6), reaction_coef, len(REACTANTS))
 
 
 class WurtzReaction(_Reaction):
     def __init__(self, initial_materials, initial_solutes, reactants=REACTANTS, products=PRODUCTS, materials=ALL_MATERIALS,
-                 solutes=SOLUTES, desired=None, de: De = wurtz_de, solver='newton', overlap=False, nmax=None, max_mol=2, thresh=1e-8, Ti=0.0,
+                 solvents=SOLVENTS, desired=None, de: De = wurtz_de, solver='newton', overlap=False, nmax=None, max_mol=2, thresh=1e-8, Ti=0.0,
                  Tmin=0.0, Tmax=0.0, dT=0.0, Vi=0.0, Vmin=0.0, Vmax=0.0, dV=0.0, dt=0):
         super(WurtzReaction, self).__init__(initial_materials, initial_solutes, reactants, products, materials,
-                                       de, solver, solutes, desired, overlap, nmax, max_mol, thresh, Ti, Tmin, Tmax, dT,
+                                       de, solver, solvents, desired, overlap, nmax, max_mol, thresh, Ti, Tmin, Tmax, dT,
                                        Vi, Vmin, Vmax, dV, dt)
 
         self.name = 'wurtz_reaction'
