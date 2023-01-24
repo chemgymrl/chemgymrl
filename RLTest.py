1c5c3365c633e3c8bcd65ce9562f7061379c02c7
ksprague1 <spragueboy@hotmail.com>
2023-01-24 14:19:58 -0500

    Added in heuristic policy for wurtz react to be run with RLTest.py

--- RLTest.py
+++ RLTest.py
@@ -3,25 +3,54 @@ import pandas as pd
 print(ALGO)
 
 
+
+class WurtzReactHeuristic():
+  def predict(s):
+    t = np.argmax(s[-7:])
+    #targs = {0: "dodecane", 1: "5-methylundecane", 2: "4-ethyldecane", 3: "5,6-dimethyldecane", 4: "4-ethyl-5-methylnonane", 5: "4,5-diethyloctane", 6: "NaCl"}
+    actions=np.array([
+    [1,0,1,0,0,1],#dodecane
+    [1,0,1,1,0,1],#5-methylundecane
+    [1,0,1,0,1,1],#4-ethyldecane
+    [1,0,0,1,0,1],#5,6-dimethyldecane
+    [1,0,0,1,1,1],#4-ethyl-5-methylnonane
+    [1,0,0,0,1,1],#4,5-diethyloctane
+    [1,0,1,1,1,1],#NaCl
+    ],dtype=np.float32)
+    return actions[t],{}
+
+
+HEURISTICS = {"WRH":WurtzReactHeuristic}
+
+
 if __name__=="__main__":
     #get options from settings.txt
     print(sys.argv[1:])
-    op=Opt()
-    op.from_file(sys.argv[1]+"/settings.txt")
-    print(op)
     
-    #Load the model
-    model = ALGO[op.algorithm].load(sys.argv[1]+"/best_model.zip")
-    print(model)
+    op=Opt()
+    #Load settings from a file but if no setting exist the user may be specifying a heuristic policy
+    try:
+        op.from_file(sys.argv[1]+"/settings.txt")
+        print(op)
+    except:
+        os.makedirs(sys.argv[1], exist_ok=True)
+    
+    #Allow user to change settings (like steps)
+    op.apply(sys.argv[1:])
+    
+    #choose algorithm
+    if op.algorithm in HEURISTICS:
+        model = HEURISTICS[op.algorithm]
+    else:
+        #Load the model
+        model = ALGO[op.algorithm].load(sys.argv[1]+"/best_model.zip")
+        print(model)
     
     
     #make the environment
     env = gym.make(op.environment)
     print("The action space is", env.action_space)
     print("The observation space is", env.observation_space)
-    
-    
-    if len(sys.argv)>2:op.steps=int(sys.argv[2])
     
     rollout = {"InState":[],"Action":[],"Reward":[],"OutState":[],"Done":[],"Info":[],"Step":[]}
     obs=env.reset()
