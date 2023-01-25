291bb01d37923b3d169915ff6897b8064fa1eac5
ksprague1 <spragueboy@hotmail.com>
2023-01-25 09:24:40 -0500

    Renamed UnitTest to QATest

--- QATest.py
+++ QATest.py
@@ -1,3 +1,29 @@
+"""
+This file is part of ChemGymRL.
+
+ChemGymRL is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ChemGymRL is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ChemGymRL.  If not, see <https://www.gnu.org/licenses/>.
+
+Module to run a gym with a random agent to ensure the code runs without error.
+
+:title: QATest.py
+
+:author: Kyle Sprague
+
+:history: 22-06-2020
+"""
+
+
 import os
 import sys
 
