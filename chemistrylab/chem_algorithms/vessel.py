4667a7aae09b7fd12f8799991d5ae1e7f95564ef
ksprague1 <spragueboy@hotmail.com>
2023-03-08 19:15:27 -0500

    removed alt print statement  indicating the boiling action has been chosen

--- chemistrylab/chem_algorithms/vessel.py
+++ chemistrylab/chem_algorithms/vessel.py
@@ -572,7 +572,7 @@ class Vessel:
                 # calculate the change in vessel temperature if all heat is used
                 vessel_temp_change = heat_available / total_entropy
 
-                print("Raising Boil Vessel Temperature by {} Kelvin".format(vessel_temp_change))
+                #print("Raising Boil Vessel Temperature by {} Kelvin".format(vessel_temp_change))
 
                 # modify the boil vessel's temperature accordingly
                 self._update_temperature(
