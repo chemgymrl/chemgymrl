535e806daffbf0b905ffc7d890e31041b6d67ca5
ksprague1 <spragueboy@hotmail.com>
2023-05-08 20:22:12 -0400

    Brainstorming how to use a shelf

--- chemistrylab/lab/shelf.py
+++ chemistrylab/lab/shelf.py
@@ -17,151 +17,48 @@ along with ChemGymRL.  If not, see <https://www.gnu.org/licenses/>.
 import sys
 import numpy as np
 sys.path.append("../../") # to access `chemistrylab`
-from chemistrylab.chem_algorithms import material, util
+from chemistrylab.chem_algorithms import material
 from chemistrylab.chem_algorithms.vessel import Vessel
 
 
 class Shelf:
     """
-    The shelf class is just as it sounds, it is a class to hold vessels from experiments
+    The shelf class holds vessels from experiments.
+    
+    pop, __getitem__, __delitem__, and append are implemented so shelves can be used similar to a list of vessels
     """
-    def __init__(self, max_num_vessels: int, vessel_paths: [np.array, list] = None):
+    def __init__(self, *args,**kwargs):
         """
-        initializes the shelf class
-        Parameters
-        ----------
-        max_num_vessels: int: the maximum number of vessels that the shelf can store
-        vessel_paths: a list of paths to vessel pickle files that are to be initialized into the shelf
+        TODO: Allow the starting vessels to be given as arguments.
         """
-        # the next available slot for inserting a vessel
-        self.open_slot = 0
-        # maximum number of vessels that can be stored in the shelf
-        self.max_num_vessels = max_num_vessels
-        # a list that keeps all the vessels
         self.vessels = []
-        if vessel_paths:
-            for i, path in enumerate(vessel_paths):
-                # function to load vessels from the path
-                self.vessels[i] = Vessel(label=i)
-                self.vessels[i].load_vessel(path)
-            self.open_slot = len(vessel_paths)
+        self.n_working = 0
 
-    def create_new_vessel(self):
-        """
-        This function simple creates a new vessel and adds it to the shelf
-        Returns
-        -------
-        None
-        """
-        index = self.open_slot
-        assert index < self.max_num_vessels
-        self.vessels.append(Vessel(label=index))
-        self.open_slot += 1
 
-    def _add_vessel_to_shelf(self, vessel):
-        """
-        This is a private member function that is used to add a vessel to the shelf
-        Parameters
-        ----------
-        vessel: the vessel that is to be added to the shelf
+    def get_working_vessels(self):
+        """Returns a tuple of the 'working' vessels used for observations and rewards."""
+        return tuple(self.vessels[:self.n_working])
 
-        Returns
-        -------
-        None
-        """
-        assert self.open_slot < self.max_num_vessels
-        vessel.label = self.open_slot
+    def pop(index=-1):
+        return self.vessels.pop(index)
+    def __getitem__(self, slice):
+        return self.vessels[slice]
+    def __delitem__(self, slice):
+        del self.vessels[slice]
+    def append(self,vessel: Vessel):
         self.vessels.append(vessel)
-        self.open_slot += 1
-
-    def get_vessel(self, index):
-        """
-        a function that returns a vessel from the shelf and removes it from the shelf
-        Parameters
-        ----------
-        index: the index of the vessel that the user wishes to retrieve from the shelf
-
-        Returns
-        -------
-        the vessel at the specified index or the vessel that was last added to the shelf, if there are
-        no vessels in the shelf then it creates a vessel and returns it
-        """
-        if self.open_slot == 0:
-            self.create_new_vessel()
-            self.open_slot -= 1
-            return self.vessels.pop(0)
-        elif index > self.open_slot or index >= len(self.vessels):
-            self.open_slot -= 1
-            return self.vessels.pop(-1)
-        elif index < len(self.vessels):
-            self.open_slot -= 1
-            return self.vessels.pop(index)
-        else:
-            raise ValueError('you failed to consider a case nicholas')
-
-
-    def delete_vessel(self, index):
-        """
-        deletes a vessel from the shelf at the specified index
-
-        Parameters
-        ----------
-        index: the index of the vessel which is to be deleted
-
-        Returns
-        -------
-        None
-        """
-        if index < self.open_slot:
-            self.open_slot -= 1
-            self.vessels.pop(index)
-        else:
-            assert KeyError(f"{index} out of arrange for array of size {len(self.vessels)}")
-
-    def return_vessel_to_shelf(self, vessel: [Vessel, list]):
-        """
-        This function allows the user to add a vessel to the shelf after an experiment has been conducted
-
-        Parameters
-        ----------
-        vessel: [Vessel, list]: a vessel or list of vessels that are to be added to the shelf
-
-        Returns
-        -------
-        None
-
-        """
-        if type(vessel) == list:
-            for ves in vessel:
-                if type(ves) != Vessel:
-                    raise TypeError(f'{type(vessel)} is not supported')
-                self._add_vessel_to_shelf(ves)
-        elif type(vessel) == Vessel:
-            self._add_vessel_to_shelf(vessel)
-        else:
-            raise TypeError(f'{type(vessel)} is not supported')
 
     def load_vessel(self, path: str):
         """
-        a function that loads a vessel from a pickle file and places it into the shelf
-        Parameters
-        ----------
-        path: the path to the vessel that is to be loaded
-
-        Returns
-        -------
-        None
+        TODO: Implement this
+        Args:
+        - path (str): the path to the vessel that is to be loaded
         """
-        self.create_new_vessel()
-        self.vessels[-1].load_vessel(path)
+        pass
 
     def reset(self):
         """
-        a function that deletes all vessels in the shelf and resets the next slot to insert
-
-        Returns
-        -------
-        None
+        TODO: Update this along with __init__
+        Resets the shelf to it's initial state
         """
-        while len(self.vessels) > 0:
-            self.delete_vessel(0)
+        self.vessels=[]
