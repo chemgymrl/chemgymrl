006508fab3183c901055521c8a443f5804ec8776
Kyle Sprague <boilscene@gmail.com>
2023-05-27 12:11:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ import sys
 setup(name='chemistrygym',
       packages=find_packages(),
       install_requires=[
-          'gym==0.18.0',
+          'gym',
           'numpy',
           'matplotlib',
           'cmocean',
