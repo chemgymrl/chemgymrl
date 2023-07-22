55a7d391561b475156244f1f602d9da22cdf08ff
ksprague1 <spragueboy@hotmail.com>
2023-07-21 21:28:37 -0400

    Updated matplotlib rendering and custom reaction notebook

--- chemistrylab/util/Visualization.py
+++ chemistrylab/util/Visualization.py
@@ -17,116 +17,37 @@ mplstyle.use('fast')
 
 from PIL import Image,ImageDraw,ImageFont
 
-@jit(nopython=True)
-def fill_line(arr,x1,y1,x2,y2,lw):
-    dy=y2-y1
-    dx=x2-x1
-    m = (dx**2+dy**2)**0.5
 
-    # Create bounding box
-    yi = max(min(y1,y2)-lw,0)
-    yf = min(max(y1,y2)+lw,arr.shape[0])
-    xi = max(x1-lw,0)
-    xf = min(x2+lw,arr.shape[1])
-    
-    for i in range(yi,yf):
-        for j in range(xi,xf):
-            A=arr[i][j][0]/255
-            x = j-x1
-            y = i-y1
+CUSTOMCOLORS = {
 
-            #//d is the dot product
-            d=x*dx+y*dy
-            #X,Y is (x,y) - proj_{dx,dy}(x,y)
-            X = x-d*dx/m/m
-            Y = y-d*dy/m/m       
-            if (d<m*m):
-                #//Same assignment here but with the arrow body
-                A=min(A,max(-3*d/lw,1-(lw/3.2-(X*X+Y*Y)**0.5)/0.4))
-            arr[i][j] = max(A,0)*255
+    "CCCCCC":           np.array([222, 229, 166],dtype=np.uint8),
+    "O":                np.array([136, 194, 229],dtype=np.uint8),
+    "CCOCC":            np.array([166, 229, 136],dtype=np.uint8),
+    "[Na+].[Cl-]":      np.array([212, 213, 211],dtype=np.uint8),
+    "CCCCCCCCCCCC":     np.array([188, 202, 157],dtype=np.uint8),
+    "CCCCCCC(C)CCCC":   np.array([175, 194, 132],dtype=np.uint8),
+    "CCCCCCC(CC)CCC":   np.array([163, 187, 106],dtype=np.uint8),
+    "CCCCC(C)C(C)CCCC": np.array([159, 198, 70 ],dtype=np.uint8),
+    "CCCCC(C)C(CC)CCC": np.array([172, 209, 31 ],dtype=np.uint8),
+    "CCCC(CC)C(CC)CCC": np.array([167, 213, 0  ],dtype=np.uint8),
+    "CCCCCCCl":         np.array([202, 212, 167],dtype=np.uint8),
+    "CCCCC(C)Cl":       np.array([189, 197, 158],dtype=np.uint8),
+    "CCCC(CC)Cl":       np.array([172, 180, 145],dtype=np.uint8),
+}
 
-def to_rgb(x):
-  """Converts a 4-channel rgba image into a 3-channel rgb image"""
-  # assume rgb premultiplied by alpha
-  rgb, a = x[..., 1:4], x[...,0:1]
-  return 255-a+rgb
+def get_color_info(vessel):
+    tol = vessel.volume/200
+    null_color = np.ones(3,dtype=np.uint8)
+    legend = np.array([CUSTOMCOLORS.get(mat._smiles,null_color) for mat in vessel._layer_mats]+[null_color*255])
+    def color_hash(indices):
+        return legend[indices]
+    cvals = np.array([CUSTOMCOLORS.get(mat._smiles,null_color) for mat in vessel._layer_mats if mat.volume_L>tol]+[null_color*255])
+    cnames = [mat._name for mat in vessel._layer_mats if mat.volume_L>tol]+["air"]
+    im = np.zeros([1,100,3],dtype=np.uint8)
+    vessel.get_layers()
+    im[:] = color_hash(vessel._hashed_layers[::-1])
 
-class numbaVisualizer():
-    def __init__(self, char_bench):
-        self.char_bench = char_bench
-        self.viz=dict(
-            spectra=self.render_spectra,
-            layers=self.render_layers,
-            PVT=self.render_PVT,
-        )
-
-        self.h=360
-        self.targets = {t:self.render_target(t) for t in char_bench.targets}
-
-        # Pre-render the text for PVT information
-        pvt = Image.new("RGBA", (self.h//2,self.h), (255,255,255))
-        draw = ImageDraw.Draw(pvt)
-        font = ImageFont.truetype("arial.ttf", self.h//8)
-        draw.text((self.h//15, 0), "T  V  P", (0,0,0), font=font)
-        self.i=0
-        self.pvt=[np.asarray(pvt)[:,:,:3].copy() for k in range(char_bench.n_vessels)]
-        
-    def get_rgb(self, vessels: Tuple[Vessel]):
-        obs_list = self.char_bench.observation_list
-        info = [self.targets[self.char_bench.target]] if "targets" in obs_list else []
-        for v in vessels:
-            info+= [self.viz[s](v) for s in obs_list if s in self.viz]
-        return np.concatenate(info,axis=1)
-    def render_layers(self, vessel: Vessel):
-        im = np.zeros([120,60,3],dtype=np.uint8)+255
-
-        im[10:115,5:55] = 0
-        im[10:110,10:50] = vessel.get_layers()[::-1,None,None]*255
-        im[10:110,10:50,1:] //= 2
-
-        im[10:110,10:50,1:]+=100
-        
-        div = self.h//120
-        return im.repeat(div, axis=0).repeat(div, axis=1)
-
-    def render_spectra(self, vessel: Vessel):
-        spectrum = np.clip(self.char_bench.get_spectra(vessel),0,0.99)
-
-        im = np.zeros([self.h,self.h,3],dtype=np.uint8)+255
-        offset=self.h//12
-        max_height = self.h-2*offset
-        scale = self.h//120
-        for x in range(99):
-            y0,y1 = int(spectrum[x]*max_height),int(spectrum[x+1]*max_height)
-
-            fill_line(im[:,:,:2], x*scale+offset, max_height-y0+offset, (x+1)*scale+offset, max_height-y1+offset, scale+1)
-
-            #im[max_height-y1+offset:max_height-y0+offset,x*scale+offset:(x+1)*scale+offset,:2] = 0
-        #fill_line(im[:,:,:2],20,20,self.h-20,20,4)
-        return im
-
-    def render_PVT(self, vessel: Vessel):
-        
-        im = self.pvt[self.i]
-        self.i=(self.i+1)%self.char_bench.n_vessels
-        offset=self.h//8
-        w=self.h//12
-        im[offset:]=255
-        max_height = self.h-self.h//6
-
-        t,v,p = 1-self.char_bench.encode_PVT(vessel)
-        im[int(max_height*t)+offset:max_height+offset,0:w*2,1:] = 0
-        im[int(max_height*v)+offset:max_height+offset,w*2:w*4,1::2] = 0
-        im[int(max_height*p)+offset:max_height+offset,w*4:w*6,3:] = 0
-        return im
-    
-    def render_target(self,target):
-        h=self.h
-        image = Image.new("RGBA", (h,h//6), (255,255,255))
-        draw = ImageDraw.Draw(image)
-        font = ImageFont.truetype("arial.ttf", h//10)
-        draw.text((5, -2), target, (0,0,0), font=font)
-        return np.asarray(image).transpose(1,0,2)[:,::-1,:3]
+    return cnames, cvals, im
 
 class matplotVisualizer():
 
@@ -212,21 +133,36 @@ class matplotVisualizer():
 
         first=first and self.steps%matplotVisualizer.legend_update_delay==0
         if prev is None or first:ax.clear()
+
+        if CUSTOM_COLORS:cnames, cvals, im = get_color_info(vessel)
+        im = im.transpose(1,0,2)
+
         if first:
-            cvals = (np.array([mat._color for mat in vessel._layer_mats]+[0.65])+0.2)%1
-            im = ax.imshow([cvals],cmap=cmap,vmin=0,vmax=1)
-            colors = [ im.cmap(im.norm(value)) for value in cvals]
-            patches = [ mpatches.Patch(color=colors[i],label= name) for i,name in enumerate(list(vessel._layer_mats)+["air"]) ] 
-            ax.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0. )
+            if CUSTOM_COLORS:
+                patches = [ mpatches.Patch(color=cvals[i].astype(np.float32)/255,label= name) for i,name in enumerate(cnames) ] 
+                ax.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0. )
+            else:
+                cvals = (np.array([mat.transmittance for mat in vessel._layer_mats]+[0.65])+0.2)%1
+                im = ax.imshow([cvals],cmap=cmap,vmin=0,vmax=1)
+                colors = [ im.cmap(im.norm(value)) for value in cvals]
+                patches = [ mpatches.Patch(color=colors[i],label= name) for i,name in enumerate(list(vessel._layer_mats)+["air"]) ] 
+                ax.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0. )
         
 
         if prev is None or first:
-            prev = ax.imshow((np.stack([layers]).T[::-1] +0.2)%1,vmin=0,vmax=1,cmap=cmap,animated=True,aspect=0.025)
+            if CUSTOM_COLORS:
+                prev = ax.imshow(im,animated=True,aspect=0.025)
+            else:
+                prev = ax.imshow((np.stack([layers]).T[::-1] +0.2)%1,vmin=0,vmax=1,cmap=cmap,animated=True,aspect=0.025)
             ax.set_xticks([])
             ax.set_yticks([])
             ax.set_xlabel(str(vessel))
         else:
-            prev.set_array((np.stack([layers]).T[::-1] +0.2)%1)
+            if CUSTOM_COLORS:
+                print(im.shape)
+                prev.set_array(im)
+            else:
+                prev.set_array((np.stack([layers]).T[::-1] +0.2)%1)
 
         return prev
 
@@ -282,26 +218,24 @@ class matplotVisualizer():
         for j,v in enumerate(vessels):
           for i,func in enumerate(observation_list):
             visual.viz[func](v,axs[i*col+j], first = j==0)
-        fig.tight_layout()
-        plt.show()
+        
+        return fig
 
+    @staticmethod
+    def display_vessels_rows(vessels: Tuple[Vessel],observation_list: Tuple[str]):
+        characterization_bench = CharacterizationBench(observation_list,[""],len(vessels))        
+        visual = Visualizer(characterization_bench)    
+        widths = visual.w*np.ones(3)*1.8#**2/np.array([visual.heights[a] for a in observation_list])
+        col = len(observation_list)
+        row=len(vessels)
+        fig,axs = plt.subplots(figsize=(widths.sum()*RES, visual.w*row*RES), nrows=row, ncols=col, width_ratios=widths,dpi=100)
+        axs=[axs] if row*col==1 else axs.flatten()
+        for i,v in enumerate(vessels):
+          for j,func in enumerate(observation_list):
+            visual.viz[func](v,axs[i*col+j], first = i==0)
 
-CUSTOMCOLORS = {
+        return fig
 
-    "CCCCCC":           np.array([222, 229, 166],dtype=np.uint8),
-    "O":                np.array([136, 194, 229],dtype=np.uint8),
-    "CCOCC":            np.array([166, 229, 136],dtype=np.uint8),
-    "[Na+].[Cl-]":      np.array([212, 213, 211],dtype=np.uint8),
-    "CCCCCCCCCCCC":     np.array([188, 202, 157],dtype=np.uint8),
-    "CCCCCCC(C)CCCC":   np.array([175, 194, 132],dtype=np.uint8),
-    "CCCCCCC(CC)CCC":   np.array([163, 187, 106],dtype=np.uint8),
-    "CCCCC(C)C(C)CCCC": np.array([159, 198, 70 ],dtype=np.uint8),
-    "CCCCC(C)C(CC)CCC": np.array([172, 209, 31 ],dtype=np.uint8),
-    "CCCC(CC)C(CC)CCC": np.array([167, 213, 0  ],dtype=np.uint8),
-    "CCCCCCCl":         np.array([202, 212, 167],dtype=np.uint8),
-    "CCCCC(C)Cl":       np.array([189, 197, 158],dtype=np.uint8),
-    "CCCC(CC)Cl":       np.array([172, 180, 145],dtype=np.uint8),
-}
 
 
 
@@ -511,7 +445,7 @@ class pygameVisualizer():
 
         pygame.draw.lines(self.surf, points=bbox, closed=True, color=(0, 0, 0))
 
-__backends = dict(numba=numbaVisualizer,matplotlib=matplotVisualizer, pygame=pygameVisualizer)
+__backends = dict(matplotlib=matplotVisualizer, pygame=pygameVisualizer)
 try:
     import pygame
     __backend="pygame"
@@ -523,7 +457,7 @@ def set_backend(backend: str):
     if backend in __backends:
         __backend = backend
     else:
-        __backend = "numba"
+        __backend = "pygame"
 
 def use_mpl_dark(size=2):
     global RES
